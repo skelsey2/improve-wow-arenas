6ec062e32a001fa07274d9887e69ba9b5f20351a
mclacore <michael@massdriver.cloud>
2022-10-30 23:40:14 -0600

    Initial files

---
+++ Locale/enUS.lua
@@ -0,0 +1,11 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("MatchAnalyzer", "enUS", true)
+
+L["Date"] = true
+L["Previous session"] = true
+L["Current session"] = true
+L["Session"] = true
+
+L["Hide minimap button"] = true
+L["Display server names"] = true
+L["Force 24-hour time format"] = true
+L["Display 24-hour timestamps even if US realm is detected."] = true
